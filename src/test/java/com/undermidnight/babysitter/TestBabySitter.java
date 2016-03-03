d65bc7ce2b835512ada2f520f20da0d3e111fb2b
Undermidnight <jhissong@gmail.com>
2016-03-03 00:56:09 -0500

    All night pay, exception cases. Finished.

--- src/test/java/com/undermidnight/babysitter/TestBabySitter.java
+++ src/test/java/com/undermidnight/babysitter/TestBabySitter.java
@@ -25,10 +25,6 @@ public class TestBabySitter extends TestCase {
 		// Test start time to bedtime
 		pay = babySitter.calculatePay(5, bedTime);
 		assertEquals(48, pay);
-		
-		// Test exception case... (prior to start time of 5 pm)
-		pay = babySitter.calculatePay(4, 6);
-		assertEquals(12, pay);
 	}
 	
 	@Test
@@ -40,6 +36,10 @@ public class TestBabySitter extends TestCase {
 		// Test two hours
 		pay = babySitter.calculatePay(10, 12);
 		assertEquals(16, pay);
+		
+		// Test bedtime to midnight
+		pay = babySitter.calculatePay(bedTime, 12);
+		assertEquals(24, pay);
 	}
 	
 	@Test
@@ -51,15 +51,53 @@ public class TestBabySitter extends TestCase {
 		// Test four hours
 		pay = babySitter.calculatePay(12, 16);
 		assertEquals(64, pay);
+	}
+	
+	@Test
+	public void testFullNightsWork() throws Exception {
 		
-		// Test exception case... (submitting hours after 4 am)
+		// All night work... 
+		int pay = babySitter.calculatePay(5, 16);
+		assertEquals(136, pay);
+		
+	}
+	
+	@Test
+	public void testMixedHoursWork() throws Exception {
+		// 7pm to 2am
+		int pay = babySitter.calculatePay(7, 14);
+		assertEquals(80, pay);
+		
+		pay = babySitter.calculatePay(11, 15);
+		assertEquals(56, pay);
+	}
+	
+	@Test
+	public void testExceptionCases() throws Exception
+	{
+		// Prior to start time of 5 pm
+		int pay = babySitter.calculatePay(4, 6);
+		assertEquals(12, pay);	
+		
+		// Submitting hours after 4 am
 		pay = babySitter.calculatePay(12, 18);
 		assertEquals(64, pay);
+		
+		// More hours than the allowed window
+		pay = babySitter.calculatePay(4, 18);
+		assertEquals(136, pay);
+		
+		// Odd numbers.. this one would only give the max pay
+		pay = babySitter.calculatePay(-800, 800);
+		assertEquals(136, pay);
+		
+		// Return zero
+		pay = babySitter.calculatePay(800, -800);
+		assertEquals(0, pay);
+		
+		// Return zero
+		pay = babySitter.calculatePay(-800, -800);
+		assertEquals(0, pay);
+		
 	}
-
-//	
-//	@Test
-//	public void testFullNightsWork() throws Exception {
-//		
-//	}
 }
